a4832ac6f30e4010076d403a043502a7dd4030ca
Simon HEGE <simonhege@gmail.com>
2022-11-12 16:16:39 +0100

    Dump request in response

--- main.go
+++ main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
 )
 
 func main() {
-        log.Print("starting server...")
-        http.HandleFunc("/", handler)
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
 
-        // Determine port for HTTP service.
-        port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-                log.Printf("defaulting to port %s", port)
-        }
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
 
-        // Start HTTP server.
-        log.Printf("listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
-                log.Fatal(err)
-        }
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        name := os.Getenv("NAME")
-        if name == "" {
-                name = "World"
-        }
-        fmt.Fprintf(w, "Hello %s!\n", name)
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
+
+	b, err := httputil.DumpRequest(r, false)
+	if err == nil {
+		w.Write(b)
+	}
 }
