9ae6c9677062cd32461a7d5ac60080f0cc2bf958
Simon HEGE <simonhege@gmail.com>
2022-11-13 06:51:33 +0100

    Connect and ping mongodb

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/xdbsoft/mongosm"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
+	uri := os.Getenv("MONGODB_URI")
+
+	log.Print("connecting to datastore")
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected and pinged.")
+
 	log.Print("starting server...")
 	http.HandleFunc("/", handler)
 
